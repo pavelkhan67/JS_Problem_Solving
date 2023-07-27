8793a319b8efa97f5e880ba3a98b636dcb3bc56a
Pavel Khan <pvelkhantopu@gmail.com>
2023-07-27 08:31:37 +0600

    Some Changes

--- Task5.js
+++ Task5.js
@@ -1,21 +1,22 @@
 // Implement a simple JavaScript calculator. The calculator should take two numbers and an operator (+, -, *, /) as input and return the result of the operation.
 
 function calculator(num1, num2, operator) {
-  switch (operator) {
-    case '+':
-      return num1 + num2;
-    case '-':
-      return num1 - num2;
-    case '*':
-      return num1 * num2;
-    case '/':
-      if (num2 === 0) {
-        return "Cannot divide by zero!";
-      }
-      return num1 / num2;
-    default:
-      return "Invalid operator";
+  if (operator === '+') {
+    return num1 + num2;
+  } else if (operator === '-') {
+    return num1 - num2;
+  } else if (operator === '*') {
+    return num1 * num2;
+  } else if (operator === '/') {
+    if (num2 === 0) {
+      return "Cannot divide by zero!";
+    }
+    return num1 / num2;
+  } else {
+    return "Invalid operator";
   }
 }
+
 const result = calculator(10, 4, '+');
 console.log("Result:", result);
+
